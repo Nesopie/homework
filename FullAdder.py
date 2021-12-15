1b2dae939952707d06732ecc85a08cef2d663b6d
Ayman <yaoiotakusama@gmail.com>
2021-12-15 23:23:36 +0530

    final draft?

--- FullAdder.py
+++ FullAdder.py
@@ -1,10 +1,11 @@
-class FullAdderCircuit : 
-    def __init__(self, firstNumber, secondNumber) : 
+class FullAdderCircuit :
+    def __init__(self, firstNumber, secondNumber, bitSize) :
         self.firstNumber = self.convertToBinary(firstNumber)
         self.secondNumber = self.convertToBinary(secondNumber)
+        self.bitSize = bitSize
         self.result = []
 
-    def add(self) : 
+    def add(self) :
         carry = 0
         firstNumberIterator = 0
         secondNumberIterator = 0
@@ -15,33 +16,33 @@ class FullAdderCircuit :
         while(firstNumberIterator < len(self.firstNumber) or secondNumberIterator < len(self.secondNumber) or carry) :
             firstDigit = 0 if firstNumberIterator >= len(self.firstNumber) else self.firstNumber[firstNumberIterator]
             secondDigit = 0 if secondNumberIterator >= len(self.secondNumber) else self.secondNumber[secondNumberIterator]
-            
+
             sum, carry = FullAdder(firstDigit, secondDigit, carry).getResult()
 
             self.result = [*self.result, sum]
 
             firstNumberIterator += 1
             secondNumberIterator += 1
-        
-        if(len(self.result) > 4) : 
-            self.result = self.result[0:4]
-        while(len(self.result) < 4) :
-            self.result = [*self.result,0]
+
+        if(len(self.result) > self.bitSize) :
+            self.result = self.result[0 : self.bitSize]
+        while(len(self.result) < self.bitSize) :
+            self.result = [*self.result , 0]
 
         self.result = self.result[::-1]
 
-    def convertToBinary(self, numberInString) : 
+    def convertToBinary(self, numberInString) :
         binary = []
         i = 0
         while(i < len(numberInString)) :
             binary = [*binary, int(numberInString[i])]
             i += 1
-        
-        return binary[::-1]
-        
 
-class FullAdder:
-    def __init__(self, firstDigit, secondDigit, carry) : 
+        return binary[::-1]
+
+
+class FullAdder :
+    def __init__(self, firstDigit, secondDigit, carry) :
         self.firstDigit = firstDigit
         self.secondDigit = secondDigit
         self.carry = carry
@@ -51,10 +52,10 @@ class FullAdder:
         carry = (self.firstDigit & self.secondDigit) | (self.secondDigit & self.carry) | (self.carry & self.firstDigit)
         return sum, carry
 
-    
-firstNumber = input("Enter the first number in binary: ")
-secondNumber = input("Enter the second number in binary: ")
+if __name__ == "__main__" :
+    firstNumber = input("Enter the first number in binary: ")
+    secondNumber = input("Enter the second number in binary: ")
 
-FullAdderObject = FullAdderCircuit(firstNumber, secondNumber)
-FullAdderObject.add()
-print(FullAdderObject.result)
+    FullAdderObject = FullAdderCircuit(firstNumber, secondNumber, 8)
+    FullAdderObject.add()
+    print(FullAdderObject.result)
